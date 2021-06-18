caeedae111700b849f6b8a484946e1805ad20329
Rhijisha5549 <75037525+Rhijisha5549@users.noreply.github.com>
2021-06-18 22:17:23 +0530

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    class Calculator
+    {
+        public double a;
+        public double b;
+       
+        public void add()
+        {
+            Console.WriteLine(a + b);
+        }
+        public void sub()
+        {
+            Console.WriteLine(a - b);
+        }
+        public void multiply()
+        {
+            Console.WriteLine(a * b);
+        }
+        public void devided()
+        {
+            Console.WriteLine(a / b);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            Console.WriteLine("first value");
+            cal.a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("second value");
+            cal.b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("New value is");
+            cal.add();
+            cal.sub();
+            Console.ReadKey();
+        }
+    }
+}
