eb4153f6ce415c8d72c760f0af670f6a7b770d5b
Rhijisha5549 <75037525+Rhijisha5549@users.noreply.github.com>
2021-07-11 13:30:18 +0530

    Add files via upload

---
+++ RestaurantTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public class RestaurantTest
+{
+    private const double V = .15;
+    public static void Main(string[] args)
+    {
+        Restaurant obj = new Restaurant("Dairy Queen", "123 Main", "V");
+        obj.GenerateReceipt();
+    }
+}
